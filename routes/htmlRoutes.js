fc6af3a65f7b8a994be19fcd9ada8e7ae4669614
Marie P <map40j@gmail.com>
2023-01-30 18:19:48 -0800

    updated the html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,12 @@ const path = require("path");
 
 //
 router.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../develop/public/notes.html/"));
+  res.sendFile(path.join(__dirname, "/../develop/public/notes.html"));
 });
 
 //
 router.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../develop/public/index.html/"));
+  res.sendFile(path.join(__dirname, "/../develop/public/index.html"));
 });
 
 
