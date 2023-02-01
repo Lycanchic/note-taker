6650b6d81e6c651ea728a46145894dfa0bd1dbd0
Marie P <map40j@gmail.com>
2023-01-31 18:17:24 -0800

    bug issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -51,4 +51,36 @@ router.delete('/notes/:id', (req, res) => {
     res.status(204).json(db);
 });
 
+router.route('/api/notes')
+  .get((req, res) => {
+    fs.readFile(dbPath)
+      .then(data => {
+        const allNotes = JSON.parse(data);
+        res.json(allNotes);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  })
+
+// Create new note
+  .post((req, res) => {
+    fs.readFile(dbPath)
+      .then(data => {
+        const allNotes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = allNotes.length ? allNotes[allNotes.length - 1].id + 1 : 1;
+        allNotes.push(newNote);
+        return fs.writeFile(dbPath, JSON.stringify(allNotes));
+      })
+      .then(() => {
+        res.json(allNotes);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
+
 module.exports = router;
